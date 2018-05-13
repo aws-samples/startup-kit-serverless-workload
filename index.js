b805067ed083b5ce1e9648752f1d6a135d0edb97
Rabowsky <rabowsky@amazon.com>
2018-05-13 09:20:30 -0700

    async/await handlers implemented

--- index.js
+++ index.js
@@ -16,124 +16,143 @@ const createResponse = (statusCode, body) => {
         statusCode: statusCode,
         body: body,
         headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         }
     }
 };
 
+
 // API call to create a TODO item
 
-exports.create = (event, context, callback) => {
+exports.create = async (event) => {
     
     let params = {
         TableName: tableName,
         Item: JSON.parse(event.body)
     };
+    let data;
     
-    let dbPut = (params) => { return dynamo.put(params).promise() };
-    
-    dbPut(params).then( (data) => {
-        console.log(`CREATE ITEM SUCCEEDED FOR todo_id = ${params.Item.todo_id}`);
-        callback(null, createResponse(200, `TODO item created with todo_id = ${params.Item.todo_id}\n`));
-    }).catch( (err) => { 
+    try {
+        data = await dynamo.put(params).promise();
+    }
+    catch(err) {
         console.log(`CREATE ITEM FAILED FOR todo_id = ${params.Item.todo_id}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err)); 
-    });
+        return createResponse(500, err);
+    }
+
+    console.log(`CREATE ITEM SUCCEEDED FOR todo_id = ${params.Item.todo_id}`);
+    return createResponse(200, `TODO item created with todo_id = ${params.Item.todo_id}\n`);
 };
+
 
 // API call to retrieve all TODO items
 
-exports.getAll = (event, context, callback) => {
+exports.getAll = async (event) => {
     
     let params = {
         TableName: tableName
     };
+    let data;
     
-    let dbGet = (params) => { return dynamo.scan(params).promise() };
+    try {
+        data = await dynamo.scan(params).promise();
+    }
+    catch(err) {
+        console.log(`GET ALL ITEMS FAILED, WITH ERROR: ${err}`);
+        return createResponse(500, err);
+    }
     
-    dbGet(params).then( (data) => {
-        if (!data.Items) {
-            callback(null, createResponse(404, 'ITEMS NOT FOUND\n'));
-            return;
-        }
-        console.log(`RETRIEVED ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
-        callback(null, createResponse(200, JSON.stringify(data.Items) + '\n') );
-    }).catch( (err) => { 
-        console.log(`GET ITEMS FAILED, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
-    });
+    if (!data.Items) {
+        console.log('NO ITEMS FOUND FOR GET ALL API CALL');
+        return createResponse(404, 'ITEMS NOT FOUND\n');
+    }
+
+    console.log(`RETRIEVED ALL ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
+    return createResponse(200, JSON.stringify(data.Items) + '\n');
 };
+
 
 // API calls related to active TODO items
 
-exports.getActive = (event, context, callback) => {
+exports.getActive = async (event) => {
     
     let params = {
         TableName: tableName,
         FilterExpression : 'active = :active',
         ExpressionAttributeValues : {':active' : true}
     };
+    let data;
     
-    let dbGet = (params) => { return dynamo.scan(params).promise() };
+    try {
+        data = await dynamo.scan(params).promise();
+    }
+    catch(err) {
+        console.log(`GET ACTIVE ITEMS FAILED, WITH ERROR: ${err}`);
+        return createResponse(500, err);
+    }
     
-    dbGet(params).then( (data) => {
-        if (!data.Items) {
-            callback(null, createResponse(404, 'ITEMS NOT FOUND\n'));
-            return;
-        }
-        console.log(`RETRIEVED ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
-        callback(null, createResponse(200, JSON.stringify(data.Items) + '\n') );
-    }).catch( (err) => { 
-        console.log(`GET ITEMS FAILED, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
-    });
+    if (!data.Items) {
+        console.log('NO ACTIVE ITEMS FOUND FOR GET ACTIVE API CALL');
+        return createResponse(404, 'ITEMS NOT FOUND\n');
+    }
+
+    console.log(`RETRIEVED ACTIVE ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
+    return createResponse(200, JSON.stringify(data.Items) + '\n');
 };
 
-exports.updateActive = (event, context, callback) => {
+
+exports.updateActive = async (event) => {
     
     let params = {
         TableName: tableName,
         Item: JSON.parse(event.body)
     };
+    let data;
     
-    let dbPut = (params) => { return dynamo.put(params).promise() };
+    try {
+        data = await dynamo.put(params).promise();
+    }
+    catch(err) {
+        console.log(`UPDATE ACTIVE FAILED FOR todo_id = ${params.Item.todo_id}, WITH ERROR: ${err}`);
+        return createResponse(500, err);
+    }
     
-    dbPut(params).then( (data) => {
-        console.log(`PUT ITEM SUCCEEDED FOR todo_id = ${params.Item.todo_id}`);
-        callback(null, createResponse(200, `TODO item updated with todo_id = ${params.Item.todo_id}\n`));
-    }).catch( (err) => { 
-        console.log(`PUT ITEM FAILED FOR todo_id = ${params.Item.todo_id}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err)); 
-    });
+    console.log(`UPDATE ACTIVE SUCCEEDED FOR todo_id = ${params.Item.todo_id}`);
+    return createResponse(200, `TODO item updated with todo_id = ${params.Item.todo_id}\n`);
 };
+
 
 // API calls related to complete TODO items
 
-exports.getComplete = (event, context, callback) => {
+exports.getComplete = async (event) => {
     
     let params = {
         TableName: tableName,
         FilterExpression : 'active = :active',
         ExpressionAttributeValues : {':active' : false}
     };
+    let data;
     
-    let dbGet = (params) => { return dynamo.scan(params).promise() };
+    try {
+        data = await dynamo.scan(params).promise();
+    }
+    catch(err) {
+        console.log(`GET COMPLETE ITEMS FAILED, WITH ERROR: ${err}`);
+        return createResponse(500, err);
+    }
     
-    dbGet(params).then( (data) => {
-        if (!data.Items) {
-            callback(null, createResponse(404, 'ITEMS NOT FOUND\n'));
-            return;
-        }
-        console.log(`RETRIEVED ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
-        callback(null, createResponse(200, JSON.stringify(data.Items) + '\n') );
-    }).catch( (err) => { 
-        console.log(`GET ITEMS FAILED, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
-    });
+    if (!data.Items) {
+        console.log('NO ITEMS FOUND FOR GET COMPLETE API CALL');
+        return createResponse(404, 'COMPLETE ITEMS NOT FOUND\n');
+    }
+
+    console.log(`RETRIEVED COMPLETE ITEMS SUCCESSFULLY WITH count = ${data.Count}`);
+    return createResponse(200, JSON.stringify(data.Items) + '\n');
 };
 
-exports.markComplete = (event, context, callback) => {
+
+exports.markComplete = async (event) => {
     
     let item = JSON.parse(event.body);
     item.active = false;
@@ -142,45 +161,51 @@ exports.markComplete = (event, context, callback) => {
         TableName: tableName,
         Item: item
     };
+    let data;
     
-    let dbPut = (params) => { return dynamo.put(params).promise() };
+    try {
+        data = await dynamo.put(params).promise();
+    }
+    catch(err) {
+        console.log(`MARK COMPLETE FAILED FOR todo_id = ${item.todo_id}, WITH ERROR: ${err}`);
+        return createResponse(500, err);
+    }
     
-    dbPut(params).then( (data) => {
-        console.log(`PUT ITEM SUCCEEDED FOR todo_id = ${item.todo_id}`);
-        callback(null, createResponse(200, `TODO item marked complete with todo_id = ${item.todo_id}\n`));
-    }).catch( (err) => { 
-        console.log(`PUT ITEM FAILED FOR todo_id = ${item.todo_id}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err)); 
-    });
+    console.log(`MARK COMPLETE SUCCEEDED FOR todo_id = ${item.todo_id}`);
+    return createResponse(200, `TODO item marked complete with todo_id = ${item.todo_id}\n`);
 };
 
-exports.deleteComplete = (event, context, callback) => {
+
+exports.deleteComplete = async (event) => {
     
     let params = {
         TableName: tableName,
         FilterExpression : 'active = :active',
         ExpressionAttributeValues : {':active' : false}
     };
+    let data;
     
     // there is no batch delete in the AWS SDK for DynamoDB,
     // so we must delete completed items one by one
-    let dbGet = (params) => { return dynamo.scan(params).promise() };
-    
-    dbGet(params).then( (data) => {
-        if (!data.Items) {
-            callback(null, createResponse(404, 'NO ITEMS FOUND FOR DELETION\n'));
-            return;
-        }
-        console.log(`NUMBER OF ITEMS TO DELETE = ${data.Count}`);
-        let ids = data.Items.map( (item) => { return item.todo_id; });
-        ids.forEach( (id) => deleteIndividualItem(id) );
-        callback(null, createResponse(200, `${data.Count} items submitted for deletion\n`) );
-    }).catch( (err) => { 
+    try {
+        data = await dynamo.scan(params).promise();
+    }
+    catch(err) {
         console.log(`GET ITEMS FOR DELETION FAILED, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
-    });   
+        return createResponse(500, err);
+    }
 
+    if (!data.Items) {
+        console.log('NO ITEMS FOUND FOR DELETION API CALL');
+        return createResponse(404, 'NO ITEMS FOUND FOR DELETION\n');
+    }
+
+    console.log(`NUMBER OF ITEMS TO DELETE = ${data.Count}`);
+    let ids = data.Items.map( (item) => { return item.todo_id; });
+    ids.forEach( (id) => deleteIndividualItem(id) );
+    return createResponse(200, `${data.Count} items submitted for deletion\n`);
 };
+
 
 function deleteIndividualItem(todoId) {
     
